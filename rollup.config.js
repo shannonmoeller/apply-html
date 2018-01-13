066772e35572150eb622cc7bb655d2b2ef98dda5
Shannon Moeller <me@shannonmoeller.com>
2018-01-13 14:35:25 -0700

    Fix camelCase util regex

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import pkg from './package.json';
 
 const external = Object.keys(pkg.dependencies);
 const globals = external.reduce((a, b) => {
-	const c = b.replace(/\W+([a-z])/g, g => g[1].toUpperCase());
+	const c = b.replace(/[\W_]+([a-z])/g, g => g[1].toUpperCase());
 
 	a[b] = c[0].toLowerCase() + c.slice(1);
 
