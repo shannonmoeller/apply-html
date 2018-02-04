c95fbb930d88d8aa7f4e06f874c1a70f905f8567
Shannon Moeller <me@shannonmoeller.com>
2018-02-04 12:32:39 -0700

    Readd globals config to rollup.test.js

--- rollup.test.js
+++ rollup.test.js
@@ -1,12 +1,16 @@
+import camelCase from 'lodash.camelcase';
 import istanbul from 'rollup-plugin-istanbul';
 import node from 'rollup-preset-node';
 import pkg from './package.json';
 
 export default {
-	external: Object.keys(pkg.dependencies),
 	output: {
 		format: 'iife',
 		name: 'test',
+		globals: Object.keys(pkg.dependencies).reduce(
+			(a, b) => Object.assign(a, { [b]: camelCase(b) }),
+			{}
+		),
 	},
 	plugins: [
 		...node(),
